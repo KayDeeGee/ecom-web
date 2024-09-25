de8593515b4821f667933bb6984dbf375a80400b
KayDeeGee <kylebryanbasco@gmail.com>
2024-09-26 03:28:15 +0800

    made section headers into a component

---
+++ components/SectionTitle.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="mb-5 text-center">
+        <p>{{ superTitle }}</p>
+        <h1 class="text-8xl font-bold">{{ title }}</h1>
+        <p>{{ subTitle }}</p>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    superTitle: String,
+    title: String,
+    subTitle: String
+});
+</script>
+
+<style lang="scss" scoped></style>
